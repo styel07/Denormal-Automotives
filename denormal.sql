a94e18c5b06e41c2a8d9637267bff2ec49e5b004
styel07 <eleazarcorpuz@gmail.com>
2015-10-22 19:52:02 -1000

    created another query to display all make_code, model_code, model_title and year

--- denormal.sql
+++ denormal.sql
@@ -21,5 +21,8 @@ FROM car_models
 WHERE make_code = 'VOLKS';
 
 -- In denormal.sql Create a query to list all make_code, model_code, model_title, and year from car_models where the make_code is 'LAM' (should have 136 rows)
+SELECT DISTINCT make_code, model_code, model_title, year
+FROM car_models
+WHERE make_code = 'LAM';
 
 -- In denormal.sql Create a query to list all fields from all car_models in years between 2010 and 2015 (should have 7884 rows)
